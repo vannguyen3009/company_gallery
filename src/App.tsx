037e55428142bc95b2397a21fe6475484b56b45a
Van Bich <van.nguyenthibich@allianceitsc.com>
2026-01-12 20:14:03 +0700

    Add AffiliateSummary component and route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import AffiliateSharing from "./pages/AffiliateSharing";
+import AffiliateSummary from "./pages/AffiliateSummary";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/affiliate-sharing" element={<AffiliateSharing />} />
+        <Route path="/affiliate-summary" element={<AffiliateSummary />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/AffiliateSummary.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AffiliateSummary = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Affiliate Summary</h1>
+      <p>This is the Affiliate Summary page.</p>
+      {/* Add your affiliate summary content here */}
+    </div>
+  );
+};
+
+export default AffiliateSummary;
